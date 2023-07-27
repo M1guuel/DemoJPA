46ff9c73ca5934be8d2c561e5cf4e572c602b866
Edson miguel <gueelmb@gmail.com>
2023-07-27 01:00:54 -0300

    Order, Instant, ISO 8601

--- Aula02/src/main/java/com/br/aula/aula02/entities/User.java
+++ Aula02/src/main/java/com/br/aula/aula02/entities/User.java
@@ -1,70 +1,82 @@
-package com.br.aula.aula02.entities;
+	package com.br.aula.aula02.entities;
+	
+	import jakarta.persistence.*;
+	
+	import java.io.Serializable;
+	import java.util.ArrayList;
+	import java.util.List;
+	import java.util.Objects;
+	@Entity
+	@Table(name= "tb_user")
+	public class User  implements Serializable {
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private long id;
+	    private String nome;
+	    private String email;
+	    private String password;
+	    
+	    @OneToMany(mappedBy = "client")
+	    private List<Order> orders = new ArrayList<>();
+	    
+	    public User() {
+	    }
+	
+	    public User(String nome, String email, String password) {
+	        this.id = id;
+	        this.nome = nome;
+	        this.email = email;
+	        this.password = password;
+	    }
+	
+	    public long getId() {
+	        return id;
+	    }
+	
+	    public void setId(long id) {
+	        this.id = id;
+	    }
+	
+	    public String getNome() {
+	        return nome;
+	    }
+	
+	    public void setNome(String nome) {
+	        this.nome = nome;
+	    }
+	
+	    public String getEmail() {
+	        return email;
+	    }
+	
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+	
+	    public String getPassword() {
+	        return password;
+	    }
+	
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+	    
+		public List<Order> getOrders() {
+			return orders;
+		}
+	
+	    @Override
+	    public boolean equals(Object o) {
+	        if (this == o) return true;
+	        if (!(o instanceof User user)) return false;
+	        return getId() == user.getId();
+	    }
+	
+	    @Override
+	    public int hashCode() {
+	        return Objects.hash(getId());
+	    }
 
-import jakarta.persistence.*;
-
-import java.io.Serializable;
-import java.util.Objects;
-@Entity
-@Table(name= "tb_user")
-public class User  implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    private String nome;
-    private String email;
-    private String password;
-
-    public User() {
-    }
-
-    public User(String nome, String email, String password) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.password = password;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User user)) return false;
-        return getId() == user.getId();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
-}
+	
+	    
+	}
