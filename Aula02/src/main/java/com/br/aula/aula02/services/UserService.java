ab78e3d0fd841267246855276ce861a13f794771
Edson miguel <gueelmb@gmail.com>
2023-07-31 04:19:17 -0300

    User insert

--- Aula02/src/main/java/com/br/aula/aula02/services/UserService.java
+++ Aula02/src/main/java/com/br/aula/aula02/services/UserService.java
@@ -1,14 +1,13 @@
 package com.br.aula.aula02.services;
 
-import com.br.aula.aula02.entities.User;
-import com.br.aula.aula02.repositories.UserRepositories;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.aula.aula02.entities.User;
+import com.br.aula.aula02.repositories.UserRepositories;
 
 @Service
 public class UserService {
@@ -22,6 +21,9 @@ public class UserService {
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
                 return obj.get();
+    }
+    public User insert(User obj) {
+    	return repository.save(obj);
     }
 
 }
